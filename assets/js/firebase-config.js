7581327ab4a9ee95d9505797df9e0d5383bc24b1
moneyfund <138159325+moneyfund@users.noreply.github.com>
2026-03-10 20:48:43 -0600

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,36 +1,21 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
-import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'REEMPLAZAR_API_KEY',
-  authDomain: 'REEMPLAZAR_AUTH_DOMAIN',
-  projectId: 'REEMPLAZAR_PROJECT_ID',
-  storageBucket: 'REEMPLAZAR_STORAGE_BUCKET',
-  messagingSenderId: 'REEMPLAZAR_MESSAGING_SENDER_ID',
-  appId: 'REEMPLAZAR_APP_ID'
+  apiKey: "AIzaSyCVL7tpUkyQWz_aVr9wFi2hrCBum2pLnPs",
+  authDomain: "inmo-nicaragua.firebaseapp.com",
+  projectId: "inmo-nicaragua",
+  storageBucket: "inmo-nicaragua.firebasestorage.app",
+  messagingSenderId: "735319266898",
+  appId: "1:735319266898:web:124c3b886d0eb32a25b18b",
+  measurementId: "G-DXTBSYNR95"
 };
 
-let app = null;
-let auth = null;
-let db = null;
-let firebaseError = null;
-
-try {
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  db = getFirestore(app);
-} catch (error) {
-  firebaseError = error;
-  console.error('Firebase no pudo inicializarse.', error);
-}
-
-const googleProvider = new GoogleAuthProvider();
-
-export {
-  app,
-  auth,
-  db,
-  googleProvider,
-  firebaseError
-};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
